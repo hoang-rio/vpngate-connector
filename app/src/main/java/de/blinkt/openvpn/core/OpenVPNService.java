f543fe4c3f7e0eb88c53d45245f7f7e6a8f094af
hoangnd <hoangnd@rikkeisoft.com>
2018-02-05 17:31:29 +0700

    update intent flag

--- app/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ app/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -284,10 +284,8 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
         // Let the configure Button show the Log
         Intent intent = new Intent(getBaseContext(), DetailActivity.class);
         intent.putExtra(DetailActivity.TYPE_START, DetailActivity.TYPE_FROM_NOTIFY);
-        //intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-//                Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | 
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP);
         return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
