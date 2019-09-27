e86191dfd5a660c3c37e1d31797500d3f3ed1c98
Hoàng Rio <donghoang.nguyen@outlook.com>
2019-09-27 13:06:29 +0700

    Migrate to androidx + use compileSdk 28 and minSdkVersion 16

--- app/src/main/java/vn/unlimit/vpngate/DetailActivity.java
+++ app/src/main/java/vn/unlimit/vpngate/DetailActivity.java
@@ -14,7 +14,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
