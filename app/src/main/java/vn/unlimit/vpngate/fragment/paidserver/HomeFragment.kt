cfb29f748b97c8a31103be434fb24808c3341a5a
Hoàng Rio <donghoang.nguyen@outlook.com>
2020-05-05 14:36:42 +0700

    Code Refactor

--- app/src/main/java/vn/unlimit/vpngate/fragment/paidserver/HomeFragment.kt
+++ app/src/main/java/vn/unlimit/vpngate/fragment/paidserver/HomeFragment.kt
@@ -1,4 +1,4 @@
-package vn.unlimit.vpngate.fragment.paidserver.home
+package vn.unlimit.vpngate.fragment.paidserver
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,26 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import vn.unlimit.vpngate.R
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_paid_server_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
--- app/src/main/java/vn/unlimit/vpngate/fragment/paidserver/ServersFragment.kt
+++ app/src/main/java/vn/unlimit/vpngate/fragment/paidserver/ServersFragment.kt
@@ -1,4 +1,4 @@
-package vn.unlimit.vpngate.fragment.paidserver.servers
+package vn.unlimit.vpngate.fragment.paidserver
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,26 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import vn.unlimit.vpngate.R
 
 class ServersFragment : Fragment() {
-
-    private lateinit var serversViewModel: ServersViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        serversViewModel =
-                ViewModelProviders.of(this).get(ServersViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_server_list, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
-        serversViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
