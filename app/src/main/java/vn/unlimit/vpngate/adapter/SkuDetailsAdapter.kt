04ba64f15c9fb370259a89ee8e7a61a6b2a6fa9b
Hoàng Rio <donghoang.nguyen@outlook.com>
2020-11-13 16:24:26 +0700

    Complete Display skuList in BuyDataFragment.kt TODO: Process buy sku

---
+++ app/src/main/java/vn/unlimit/vpngate/adapter/SkuDetailsAdapter.kt
@@ -0,0 +1,78 @@
+package vn.unlimit.vpngate.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.android.billingclient.api.SkuDetails
+import vn.unlimit.vpngate.R
+
+class SkuDetailsAdapter(context: Context?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    val mContext = context
+    private var onItemClickListener: OnItemClickListener? = null
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+    private var _list: ArrayList<SkuDetails> = ArrayList()
+    private var lastPosition = 0
+    fun initialize(skuList: List<SkuDetails>?) {
+        try {
+            _list.clear()
+            if (skuList != null) {
+                _list.addAll(skuList)
+            }
+            notifyDataSetChanged()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun setOnItemClickListener(_onItemClickListener: OnItemClickListener?) {
+        onItemClickListener = _onItemClickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return VHTypeSkuDetails(layoutInflater.inflate(R.layout.item_sku_details, parent, false))
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        (viewHolder as VHTypeSkuDetails).bindViewHolder(position)
+        lastPosition = position
+    }
+
+    override fun getItemCount(): Int {
+        return _list.size
+    }
+
+    inner class VHTypeSkuDetails(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private val txtTitle: TextView = itemView.findViewById(R.id.txt_title)
+        private val txtDescription: TextView = itemView.findViewById(R.id.txt_description)
+        private val btnBuy: Button = itemView.findViewById(R.id.btn_buy)
+        override fun onClick(v: View?) {
+            try {
+                if (onItemClickListener != null) {
+                    val item: SkuDetails = _list[adapterPosition]
+                    onItemClickListener!!.onItemClick(item, adapterPosition)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        fun bindViewHolder(position: Int) {
+            try {
+                val skuDetails: SkuDetails = _list.get(position)
+                txtTitle.text = skuDetails.title
+                txtDescription.text = skuDetails.description
+                btnBuy.text = mContext!!.getString(R.string.btn_buy_data, skuDetails.price)
+            } catch (th: Throwable) {
+                th.printStackTrace()
+            }
+        }
+
+        init {
+            btnBuy.setOnClickListener(this)
+        }
+    }
+}
--- app/src/main/java/vn/unlimit/vpngate/fragment/paidserver/BuyDataFragment.kt
+++ app/src/main/java/vn/unlimit/vpngate/fragment/paidserver/BuyDataFragment.kt
@@ -5,24 +5,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.billingclient.api.*
+import vn.unlimit.vpngate.App
 import vn.unlimit.vpngate.R
-
+import vn.unlimit.vpngate.adapter.OnItemClickListener
+import vn.unlimit.vpngate.adapter.SkuDetailsAdapter
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * A simple [Fragment] subclass.
  * Use the [BuyDataFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BuyDataFragment : Fragment(), View.OnClickListener {
+class BuyDataFragment : Fragment(), View.OnClickListener, OnItemClickListener {
     private var btnBack: ImageView? = null
+    private var listSkus: Array<String>? = null
+    private var dataUtil = App.getInstance().dataUtil
+    private var billingClient: BillingClient? = null
+    private var lnLoading: View? = null
+    private var rcvSkuDetail: RecyclerView? = null
+    private var skuDetailsAdapter: SkuDetailsAdapter? = null
+    private val purchasesUpdatedListener =
+            PurchasesUpdatedListener { billingResult, purchases ->
+                // To be implemented in a later section.
+            }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        billingClient = BillingClient.newBuilder(requireActivity())
+                .setListener(purchasesUpdatedListener)
+                .enablePendingPurchases()
+                .build()
+        initBilling()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_buy_data, container, false)
         btnBack = root.findViewById(R.id.btn_back)
         btnBack?.setOnClickListener(this)
+        lnLoading = root.findViewById(R.id.ln_loading_wrap)
+        rcvSkuDetail = root.findViewById(R.id.rcv_sku_details)
+        rcvSkuDetail!!.layoutManager = LinearLayoutManager(context)
+        skuDetailsAdapter = SkuDetailsAdapter(context)
+        skuDetailsAdapter!!.setOnItemClickListener(this)
+        rcvSkuDetail!!.adapter = skuDetailsAdapter
         return root
     }
 
@@ -31,8 +65,53 @@ class BuyDataFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v) {
+        when (v) {
             btnBack -> findNavController().popBackStack()
         }
+    }
+
+    private fun initBilling() {
+        if (dataUtil.hasAds()) {
+            listSkus = resources.getStringArray(R.array.free_paid_skus)
+        } else {
+            listSkus = resources.getStringArray(R.array.pro_paid_skus)
+        }
+        billingClient?.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    querySkuDetails()
+                }
+            }
+
+            override fun onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        })
+    }
+
+    fun querySkuDetails() {
+        val skuList = ArrayList<String>()
+        skuList.addAll(listSkus!!)
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        billingClient?.querySkuDetailsAsync(params.build()) { result, listSkuDetails ->
+            if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                lnLoading?.visibility = View.GONE
+                rcvSkuDetail?.visibility = View.VISIBLE
+                Collections.sort(listSkuDetails!!, Comparator { skuDetails: SkuDetails, skuDetails1: SkuDetails ->
+                    return@Comparator skuDetails.priceAmountMicros.compareTo(skuDetails1.priceAmountMicros)
+                })
+                skuDetailsAdapter!!.initialize(listSkuDetails)
+            } else {
+                Toast.makeText(context, getString(R.string.get_sku_list_error), Toast.LENGTH_LONG).show()
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    override fun onItemClick(o: Any?, position: Int) {
+        val skuDetails = o as SkuDetails
+        Toast.makeText(context, "Click buy item %s at %d".format(skuDetails.sku, position), Toast.LENGTH_SHORT).show()
     }
 }
