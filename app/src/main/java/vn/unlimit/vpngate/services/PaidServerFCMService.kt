a9a25253b91d84023dd81dea7eb111cc04538c7a
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-06-28 14:07:54 +0700

    Add local notification to show notification when app open

--- app/src/main/java/vn/unlimit/vpngate/services/PaidServerFCMService.kt
+++ app/src/main/java/vn/unlimit/vpngate/services/PaidServerFCMService.kt
@@ -1,8 +1,16 @@
 package vn.unlimit.vpngate.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.common.base.Strings
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -14,6 +22,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import vn.unlimit.vpngate.App
 import vn.unlimit.vpngate.R
+import vn.unlimit.vpngate.activities.MainActivity
 import vn.unlimit.vpngate.api.DeviceApiService
 import vn.unlimit.vpngate.models.DeviceInfo
 import vn.unlimit.vpngate.models.request.DeviceAddRequest
@@ -34,10 +43,11 @@ class PaidServerFCMService : FirebaseMessagingService() {
         return@addInterceptor it.proceed(requestBuilder.build())
     }
 
-    private var retrofit: Retrofit = Retrofit.Builder().baseUrl(
-        FirebaseRemoteConfig.getInstance()
-            .getString(App.getResourceString(R.string.cfg_paid_server_api_base_url))
-    )
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(
+            FirebaseRemoteConfig.getInstance()
+                .getString(App.getResourceString(R.string.cfg_paid_server_api_base_url))
+        )
         .client(httpClientBuilder.build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
@@ -46,6 +56,66 @@ class PaidServerFCMService : FirebaseMessagingService() {
 
     companion object {
         const val TAG = "PaidServerFCMService"
+        val vibrationPattern = longArrayOf(100, 1000, 100, 1000, 100)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+            sendNotification(
+                remoteMessage.data["title"].toString(),
+                remoteMessage.data["body"].toString(),
+            )
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+
+    private fun sendNotification(title: String, messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val requestCode = 0
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE,
+        )
+
+        val channelId = getString(R.string.notification_channel_id)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setVibrate(vibrationPattern)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+
+        createNotificationChannel()
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                getString(R.string.notification_channel_id),
+                getString(R.string.notification_channel_name),
+                NotificationManager.IMPORTANCE_HIGH,
+            )
+            channel.vibrationPattern = vibrationPattern
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     @OptIn(DelicateCoroutinesApi::class)
